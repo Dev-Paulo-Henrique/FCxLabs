26848710cdec13152cb0704ac9286a0f606574eb
Dev-Paulo-Henrique <jwcfree11@gmail.com>
2022-09-28 12:38:33 -0300

    Add update and delete

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-<<<<<<< HEAD
-  baseURL: "http://localhost:3333/",
-  // baseURL: 'https://pe0mne.sse.codesandbox.io/'
-});
-=======
-  // baseURL: 'http://localhost:3333/'
+  // baseURL: "http://localhost:3333/",
   baseURL: 'https://pe0mne.sse.codesandbox.io/'
-})
->>>>>>> 2a8bb1015edb036e7bb3962f34f373a51ddc24c7
+});
